63aef9601327e6610fec4d21b71aa67c1cce9a5d
guepardo04 <69167581+guepardo04@users.noreply.github.com>
2020-08-08 20:55:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var Staff = "682605083334410338"
 var STAFF = "741582315473469532"
-
+var PREFIX = "/"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -60,55 +60,35 @@ client.on('guildMemberAdd', member => {
 
 });
 
-
 client.on('message', message => {
-  if (message.member.roles.cache.find(roles => roles.id === STAFF)) {
-    // Ignore messages that aren't from a guild
-    if (!message.guild) return;
-
-      // if the message content starts with "/ban"
-    if (message.content.startsWith('/ban')) {
-      // Assuming we mention someone in the message, this will return the user
-      const user = message.mentions.users.first();
-      // If we have a user mentioned
-      if (user) {
-        // Now we get the member from the user
-        const member = message.guild.member(user);
-        // If the member is in the guild
-        if (member) {
-         /**
-         * Ban the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-          member
-            .ban({
-            reason: 'They were bad!',
-            })
-            .then(() => {
-            // We let the message author know we were able to ban the person
-            message.reply(`has baneado a  ${user.tag}`);
-            })
-            .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to ban the member,
-            // either due to missing permissions or role hierarchy
-              message.reply('no puedo banear a este miembro');
-            // Log the error
-              console.error(err);
-            });
+  if (message.content.startsWith(PREFIX + 'ban')) {
+    if (message.member.roles.cache.find(roles => roles.id === STAFF)) {
+        const user = message.mentions.users.first();
+        if (user) {
+          const member = message.guild.member(user);
+          if (member) {
+            member
+              .ban({
+              reason: 'They were bad!',
+              })
+              .then(() => {
+                message.channel.send("has baneado a <@" + user + ">");
+              })
+              .catch(err => {
+                message.channel.send('no puedo banear a este miembro');
+                console.error(err);
+              });
+          } else {
+            message.channel.send("el usuario no esta en el server!");
+          }
         } else {
-        // The mentioned user isn't in this guild
-          message.reply("el usuario no esta en el server!");
+          message.channel.send("menciona a un usuario!");
         }
       } else {
-      // Otherwise, if no user was mentioned
-        message.reply("menciona a un usuario!");
+      message.channel.send('no tienes el permiso necesario para banear usuarios')
       }
-    }
-    } else {
-    message.reply('no tienes el permiso necesario para banear usuarios')
-}});
+  }});
+
 
 
 
