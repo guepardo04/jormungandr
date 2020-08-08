63144ee93051ec82caba2893dc48166c3c34a532
guepardo04 <69167581+guepardo04@users.noreply.github.com>
2020-08-08 10:50:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -61,52 +61,53 @@ client.on('guildMemberAdd', member => {
 
 client.on('message', message => {
   if (message.member.roles.cache.find(roles => roles.id === Staff)) {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
+    // Ignore messages that aren't from a guild
+    if (!message.guild) return;
 
-  // if the message content starts with "!ban"
-  if (message.content.startsWith('/ban')) {
-    // Assuming we mention someone in the message, this will return the user
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
+      // if the message content starts with "!ban"
+    if (message.content.startsWith('/ban')) {
+      // Assuming we mention someone in the message, this will return the user
+      const user = message.mentions.users.first();
+      // If we have a user mentioned
+      if (user) {
+        // Now we get the member from the user
+        const member = message.guild.member(user);
+        // If the member is in the guild
+        if (member) {
+         /**
          * Ban the member
          * Make sure you run this on a member, not a user!
          * There are big differences between a user and a member
          */
-        member
-          .ban({
+          member
+            .ban({
             reason: 'They were bad!',
-          })
-          .then(() => {
+            })
+            .then(() => {
             // We let the message author know we were able to ban the person
             message.reply(`has baneado a  ${user.tag}`);
-          })
-          .catch(err => {
+            })
+            .catch(err => {
             // An error happened
             // This is generally due to the bot not being able to ban the member,
             // either due to missing permissions or role hierarchy
-            message.reply('no puedo banear a este miembro');
+              message.reply('no puedo banear a este miembro');
             // Log the error
-            console.error(err);
-          });
-      } else {
+              console.error(err);
+            });
+        } else {
         // The mentioned user isn't in this guild
-        message.reply("el usuario no esta en el server!");
-      }
-    } else {
+          message.reply("el usuario no esta en el server!");
+        }
+      } else {
       // Otherwise, if no user was mentioned
-      message.reply("menciona a un usuario!");
+        message.reply("menciona a un usuario!");
+      }
     }
-  }
-  } else {
-  message.reply('no tienes el permiso necesario para banear usuarios')
+    } else {
+    message.reply('no tienes el permiso necesario para banear usuarios')
 }});
+
 
 
 
