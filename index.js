14919b55f1bbc8f1def89c0a39af5b1b5d26dceb
guepardo04 <69167581+guepardo04@users.noreply.github.com>
2020-08-08 10:41:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ client.on('message', async msg => {
   
 
 
-})
+});
 
 
 client.on('guildMemberAdd', member => {
@@ -52,53 +52,15 @@ client.on('guildMemberAdd', member => {
   channel.send(`Bienvenido al servidor, ${member}`);
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'testeo-uwu');
 
-
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
-
-  // If the message content starts with "!kick"
-  if (message.content.startsWith('/kick')) {
-    // Assuming we mention someone in the message, this will return the user
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member
-          .kick('Optional reason that will display in the audit logs')
-          .then(() => {
-            // We let the message author know we were able to kick the person
-            message.reply(`Has hechado a ${user.tag}`);
-          })
-          .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('No puedo hechar a este usuario!');
-            // Log the error
-            console.error(err);
-          });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply("El usuario no esta en el server!");
-      }
-      // Otherwise, if no user was mentioned
-    } else {
-      message.reply("Menciona a un usuario!");
-    }
-  }
 });
 
+
 client.on('message', message => {
+  if (message.member.roles.cache.find(roles => roles.id === Staff)) {
   // Ignore messages that aren't from a guild
   if (!message.guild) return;
 
@@ -142,9 +104,11 @@ client.on('message', message => {
       message.reply("menciona a un usuario!");
     }
   }
-});
+  } else {
+  message.reply('no tienes el permiso necesario para banear usuarios')
+}});
 
 
 
 
-client.login('NzM5ODAwMDIyMzc1MjAyODk2.Xyfudg.i4rS8xPZiYq8YorrpK4oGAbeSbE');
+client.login('NzQxMzcxNTQ3MjMwNTM1ODIx.Xy2mDg.8b__S4k4lfhI6J0Dfbdm1Uuax0M');
